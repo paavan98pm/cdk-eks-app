cb9b408ca0922206753c4f62de9ee18d6285e442
paavan98pm <paavan@protonmail.com>
2020-10-20 14:02:41 -0500

    create eks cluster

--- lib/cdk-eks-app-stack.ts
+++ lib/cdk-eks-app-stack.ts
@@ -1,9 +1,13 @@
 import * as cdk from '@aws-cdk/core';
+import * as eks from '@aws-cdk/aws-eks';
 
 export class CdkEksAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const cluster = new eks.Cluster(this, 'cdk-eks-cluster', {
+      version: eks.KubernetesVersion.V1_18,
+    });
   }
 }
